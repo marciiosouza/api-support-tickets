b39c097d8eb0e09242455379fc377cd2e1f83ecb
Marcio Souza <marciioasouzza@gmail.com>
2025-09-22 22:21:52 -0300

    feat: filtrando por status

--- src/database/database.js
+++ src/database/database.js
@@ -31,6 +31,15 @@ export class Database {
 
   select(table, filters) {
     let data = this.#database[table] ?? []
+
+    if (filters) {
+      data = data.filter((row) => {
+        return Object.entries(filters).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase())
+        })
+      })
+    }
+
     return data
 
   }
