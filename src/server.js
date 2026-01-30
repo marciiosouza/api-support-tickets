36da5f7594f8a3cd3c28f9b2d28fc73f4d58a032
Marcio Souza <marciioasouzza@gmail.com>
2026-01-30 16:10:28 -0300

    add log

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import http from "node:http"
 import { jsonHandler } from "./middlewares/jsonHandler.js"
 import { routeHandler } from "./middlewares/routeHandler.js"
 
+console.log('Iniciando...')
+
 async function listener(request, response) {
   await jsonHandler(request, response)
   routeHandler(request, response)
